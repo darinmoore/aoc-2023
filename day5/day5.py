447a1e738bd3e69260d9db61f869492898293a97
darinmoore <moored2@mskcc.org>
2023-12-05 09:19:31 -0500

    solves day 5 part 1

---
+++ day5/day5.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+#
+#  Advent of Code 2023 - Day 5
+#
+from typing import Sequence, Union, Optional, Any, Dict, List, Tuple
+from pathlib import Path
+from collections import defaultdict
+from dataclasses import dataclass
+import math
+import re
+
+INPUTFILE = "input.txt"
+
+SAMPLE_CASES = [
+    (
+        """
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+
+        water-to-light map:
+        88 18 7
+        18 25 70
+
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+
+        humidity-to-location map:
+        60 56 37
+        56 93 4
+        """,
+        35
+    ),
+]
+
+SAMPLE_CASES2 = [
+    (
+        """
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+
+        water-to-light map:
+        88 18 7
+        18 25 70
+
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+
+        humidity-to-location map:
+        60 56 37
+        56 93 4
+        """,
+        46
+    ),
+]
+
+
+Lines = Sequence[str]
+Sections = Sequence[Lines]
+
+# Utility functions
+
+def load_input(infile: str, strip=True, blank_lines=False) -> Lines:
+    return load_text(Path(infile).read_text())
+
+def load_text(text: str, strip=True, blank_lines=False) -> Lines:
+    if strip:
+        lines = [line.strip() for line in text.strip("\n").split("\n")]
+    else:
+        lines = [line for line in text.strip("\n").split("\n")]
+    if blank_lines:
+        return lines
+    return [line for line in lines if line.strip()]
+
+def parse_sections(lines: Lines) -> Sections:
+    result = []
+    sect = []
+    for line in lines:
+        if not line.strip():
+            if sect:
+                result.append(sect)
+            sect = []
+        else:
+            sect.append(line)
+    if sect:
+        result.append(sect)
+    return result
+
+NUM_MAPS = 7
+
+# Solution
+def parse_lines(lines):
+    seeds = lines[0].split(': ')[1].split()
+    seeds = [int(seed) for seed in seeds]
+    maps = [[] for _ in range(NUM_MAPS)]
+    i = 1
+    map_num = -1
+    while i < len(lines):
+        line = lines[i]
+        if not line[0].isdigit():
+            map_num += 1
+        else:
+            map_range = line.split()
+            maps[map_num].append([int(r) for r in map_range])
+        i += 1        
+    return seeds, maps
+
+def get_location(seed, maps):
+    for map_ranges in maps:
+        for map_range in map_ranges:
+            source = map_range[1]
+            dest   = map_range[0]
+            length = map_range[2]
+            if source <= seed < source + length:
+                seed = dest + seed - source
+                break
+    return seed
+
+def solve2(lines: Lines) -> int:
+    """Solve the problem."""
+    seeds, maps = parse_lines(lines)
+    return 0
+
+def solve(lines: Lines) -> int:
+    """Solve the problem."""
+    seeds, maps = parse_lines(lines)
+    seeds = [get_location(seed,maps) for seed in seeds]
+    return min(seeds)
+
+
+# PART 1
+
+def example1() -> None:
+    """Run example for problem with input arguments."""
+    print("EXAMPLE 1:")
+    for text, expected in SAMPLE_CASES:
+        lines = load_text(text)
+        result = solve(lines)
+        print(f"'{text}' -> {result} (expected {expected})")
+        assert result == expected
+    print("= " * 32)
+
+def part1(lines: Lines) -> None:
+    print("PART 1:")
+    result = solve(lines)
+    print(f"result is {result}")
+    print("= " * 32)
+
+
+# PART 2
+
+def example2() -> None:
+    """Run example for problem with input arguments."""
+    print("EXAMPLE 2:")
+    for text, expected in SAMPLE_CASES2:
+        lines = load_text(text)
+        result = solve2(lines)
+        print(f"'{text}' -> {result} (expected {expected})")
+        assert result == expected
+    print("= " * 32)
+
+def part2(lines: Lines) -> None:
+    print("PART 2:")
+    result = solve2(lines)
+    print(f"result is {result}")
+    print("= " * 32)
+
+
+if __name__ == "__main__":
+    example1()
+    input_lines = load_input(INPUTFILE)
+    part1(input_lines)
+    example2()
+    part2(input_lines)
